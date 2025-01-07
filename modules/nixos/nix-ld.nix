80d57acf662a767895714fc9e0de21aca48e5fa7
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2025-01-07 16:21:46 +0200

    Enable nix-ld to run unmodified binaries

---
+++ modules/nixos/nix-ld.nix
@@ -0,0 +1,63 @@
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}: {
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs;
+      [
+        zlib
+        zstd
+        stdenv.cc.cc
+        curl
+        openssl
+        attr
+        libssh
+        bzip2
+        libxml2
+        acl
+        libsodium
+        util-linux
+        xz
+        systemd
+      ]
+      ++ lib.optionals (config.hardware.graphics.enable) [
+        pipewire
+        cups
+        libxkbcommon
+        pango
+        mesa
+        libdrm
+        libglvnd
+        libpulseaudio
+        atk
+        cairo
+        alsa-lib
+        at-spi2-atk
+        at-spi2-core
+        gdk-pixbuf
+        glib
+        gtk3
+        libGL
+        libappindicator-gtk3
+        vulkan-loader
+        xorg.libX11
+        xorg.libXScrnSaver
+        xorg.libXcomposite
+        xorg.libXcursor
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXrender
+        xorg.libXtst
+        xorg.libxcb
+        xorg.libxkbfile
+        xorg.libxshmfence
+        xorg.libXinerama
+      ];
+  };
+}
