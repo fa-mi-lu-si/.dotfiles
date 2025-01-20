4a8783263b6d744a1dc46eefef465dab2f3d919b
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2025-01-20 09:19:22 +0200

    Edit kitty config

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -14,15 +14,16 @@
       tab_bar_margin_width = 0.0;
       tab_bar_margin_height = "0.0 0.0";
       tab_bar_min_tabs = 2;
-      tab_title_template = " {tab.active_exe}{bell_symbol}{activity_symbol} {f' {num_windows}' if num_windows > 1 else ''} ";
+      tab_title_template = " {tab.active_oldest_exe}{bell_symbol}{activity_symbol} {f' {num_windows}' if num_windows > 1 else ''} ";
       active_tab_title_template = " {f' ' if (layout_name == 'stack' and num_windows > 1) else ''}{title}{bell_symbol}{activity_symbol} ";
       window_padding_width = "5 10";
       remember_window_size = false;
 
+      enabled_layouts = "tall, splits:split_axis=auto, fat, grid, horizontal, vertical, stack";
+
       cursor_trail = 5;
       cursor_shape = "beam";
       cursor_stop_blinking_after = 15.0;
-      touch_scroll_multiplier = 2;
 
       # colemak dh
       visual_window_select_characters = "tnseriaoq;wyfupl";
@@ -34,6 +35,14 @@
       "ctrl+shift+n" = "launch --cwd=current";
       "ctrl+shift+w" = "close_tab";
       "ctrl+shift+f5" = "load_config_file";
+      "ctrl+shift+u" = "kitten unicode_input";
+      "ctrl+shift+f1" = "show_kitty_doc overview";
+      "ctrl+super+shift+tab" = "swap_with_window";
+      "ctrl+super+tab" = "focus_visible_window";
+
+      "ctrl+shift+l" = "next_layout";
+      "ctrl+shift+f11" = "toggle_fullscreen";
+      "ctrl+super+u" = "toggle_layout stack";
 
       "ctrl+shift+c" = "copy_to_clipboard";
       "ctrl+shift+v" = "paste_from_clipboard";
@@ -42,20 +51,13 @@
       "ctrl+down" = "scroll_to_prompt 1";
       "ctrl+shift+o" = "show_last_visited_command_output";
 
-      "ctrl+shift+l" = "next_layout";
-      "ctrl+shift+pgdn" = "next_tab";
-      "ctrl+shift+pgup" = "previous_tab";
-      "ctrl+shift+right" = "no_op";
-      "ctrl+shift+left" = "no_op";
-
-      "ctrl+shift+f11" = "toggle_fullscreen";
-      "ctrl+super+u" = "toggle_layout stack";
-
       "ctrl+super+left" = "neighboring_window left";
       "ctrl+super+right" = "neighboring_window right";
       "ctrl+super+up" = "neighboring_window up";
       "ctrl+super+down" = "neighboring_window down";
 
+      "ctrl+shift+pgdn" = "next_tab";
+      "ctrl+shift+pgup" = "previous_tab";
       "ctrl+super+pgdn" = "next_tab";
       "ctrl+super+pgup" = "previous_tab";
       "ctrl+super+shift+pgdn" = "move_tab_forward";
@@ -66,15 +68,20 @@
       "ctrl+shift+minus" = "change_font_size all -2.0";
       "ctrl+shift+0" = "change_font_size all 0";
       "ctrl+)" = "change_font_size all 0";
-
-      "ctrl+shift+u" = "kitten unicode_input";
-      "ctrl+super+shift+tab" = "swap_with_window";
-      "ctrl+super+tab" = "focus_visible_window";
-      "ctrl+shift+f1" = "show_kitty_doc overview";
     };
 
     extraConfig = ''
       map --new-mode resize ctrl+shift+r
+      map --mode resize r layout_action rotate
+      map --mode resize l next_layout
+
+      map --mode resize s goto_layout splits
+      map --mode resize g goto_layout grid
+      map --mode resize u goto_layout stack
+      map --mode resize h goto_layout fat
+      map --mode resize v goto_layout tall
+      map --mode resize shift+h goto_layout horizontal
+      map --mode resize shift+v goto_layout vertical
 
       map --mode resize up neighboring_window up
       map --mode resize left neighboring_window left
@@ -116,7 +123,7 @@
 
     protocol file
     mime text/html
-    action launch --type=background zen $FILE_PATH
+    action launch --type=background xdg-open $FILE_PATH
 
     protocol file
     mime text/*
