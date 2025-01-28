b9526fa0999ead83d3c09b4829e49b13a4df423c
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2025-01-28 22:12:25 +0200

    Fix the probe-rs module

--- modules/nixos/probe-rs/default.nix
+++ modules/nixos/probe-rs/default.nix
@@ -3,6 +3,7 @@
     (pkgs.writeTextFile {
       name = "probe-rs udev";
       text = builtins.readFile ./69-probe-rs.rules;
+      destination = "/lib/udev/rules.d/69-probe-rs.rules";
     })
   ];
 }
