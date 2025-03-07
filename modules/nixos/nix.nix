e2e7da27f10f0179c8bfcd3b39e520bf9f896ddd
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2025-03-07 13:09:41 +0200

    re-enable flakes and the nix command

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,6 +1,8 @@
 {...}: {
   nix.settings.auto-optimise-store = true;
-  # nix.gc = {
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+  }; # nix.gc = {
   #   automatic = true;
   #   options = "--delete-older-than 14d";
   # };
