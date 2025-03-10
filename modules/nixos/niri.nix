e4bf151d385bc46bafb8573855accb5bfbfa2b73
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2025-03-10 18:15:17 +0200

    Add the xdg stuff niri needs

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -1,5 +1,17 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     # inputs.niri.nixosModules.niri
   ];
+  environment.systemPackages = [pkgs.xdg-utils];
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+    ];
+  };
 }
