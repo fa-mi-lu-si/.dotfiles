7a862b2a3e82cf93e7c410c378758e84f0c202a0
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2025-04-10 21:29:27 +0200

    edit stylix theme, custom theme based on paradise and oxocarbon

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -8,23 +8,22 @@
     enable = true;
 
     base16Scheme = {
-      # Paradise
       "base00" = "#151515";
       "base01" = "#1B1B1B";
       "base02" = "#242424";
-      "base03" = "#424242";
-      "base04" = "#bbb6b6";
+      "base03" = "#393939";
+      "base04" = "#BBBEC9";
       "base05" = "#E8E3E3";
-      "base06" = "#dcdcdc";
-      "base07" = "#f0f0f0";
+      "base06" = "#DCDCDC";
+      "base07" = "#F0F0F0";
       "base08" = "#B66467";
       "base09" = "#C8907A";
       "base0A" = "#D9BC8C";
       "base0B" = "#8C977D";
       "base0C" = "#8AA6A2";
-      "base0D" = "#8DA3B9";
+      "base0D" = "#959BC3";
       "base0E" = "#A988B0";
-      "base0F" = "#C0784D";
+      "base0F" = "#BE95FF";
     };
     polarity = "dark";
 
