729cf5ab901c4aa87913176bd645363421c77be7
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2026-01-08 16:08:46 +0200

    update lockfile

--- modules/hm/desktop/vicinae.nix
+++ modules/hm/desktop/vicinae.nix
@@ -5,7 +5,13 @@
 
   services.vicinae = {
     enable = true;
-    autoStart = true;
+    systemd = {
+      enable = true;
+      autoStart = true;
+      environment = {
+        USE_LAYER_SHELL = 1;
+      };
+    };
     settings = {
       closeOnFocusLoss = true;
       faviconService = "twenty";
