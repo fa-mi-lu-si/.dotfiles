824921742e49ffac5ffbd8c6d54b5b198bafccab
Oluwaseun Samuel Familusi <56502184+fa-mi-lu-si@users.noreply.github.com>
2026-07-16 22:12:20 +0300

    fix helix yazi integration when filepath has spaces

--- modules/hm/helix/keymap.nix
+++ modules/hm/helix/keymap.nix
@@ -13,11 +13,11 @@
       space.w.d = [":vsplit" "goto_definition"];
       space.e = [
         ":sh rm -f /tmp/unique-file"
-        ":insert-output yazi %{buffer_name} --chooser-file=/tmp/unique-file"
-        '':insert-output echo "\x1b[?1049h\x1b[?2004h" > /dev/tty''
+        ":set mouse false"
+        '':insert-output yazi "%{buffer_name}" --chooser-file=/tmp/unique-file''
+        '':sh printf "\x1b[?1049h\x1b[?2004h" > /dev/tty''
         ":open %sh{cat /tmp/unique-file}"
         ":redraw"
-        ":set mouse false"
         ":set mouse true"
       ];
       X = ["extend_line_up" "extend_to_line_bounds"];
